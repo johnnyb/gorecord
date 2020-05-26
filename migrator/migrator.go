1e4c18e4d9652c7b8e5d13136aa4a298aff31f91
Jonathan Bartlett <jonathan.bartlett@specialized.com>
2020-05-25 22:38:47 -0500

    Starting to think about migrations

---
+++ migrator/migrator.go
@@ -0,0 +1,32 @@
+package migrator
+
+import (
+	"database/sql"
+	// "github.com/johnnyb/gorecord/gorec"
+)
+
+func MigrateWithDirectory() {
+	// Go through the directory in canonical order
+	// Look for files named `.up.migration`
+	// Run UpMigrateWithFile
+}
+
+func UpMigrateWithFile() {
+	// Make sure the migration table exists, create it if it doesn't
+	// Take filename and find migration identifier
+	// Read file for commands
+	// Execute commands (in transaction?)
+	// Mark the migration as done
+}
+
+func DownMigrateWithFile() {
+
+}
+
+func PerformUpMigration(db *sql.DB, identifier string, migration string) {
+
+}
+
+func PerformDownMigration(db *sql.DB, identifier string, migration string) {
+
+}
